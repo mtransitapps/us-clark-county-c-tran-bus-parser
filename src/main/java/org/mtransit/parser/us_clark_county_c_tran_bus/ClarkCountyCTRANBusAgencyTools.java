a80b4601cd3b07e2516f1260e79701d98546d7d9
Mathieu Méa <mathieu.mea@gmail.com>
2021-09-30 09:31:01 -0400

    Cleanup

--- src/main/java/org/mtransit/parser/us_clark_county_c_tran_bus/ClarkCountyCTRANBusAgencyTools.java
+++ src/main/java/org/mtransit/parser/us_clark_county_c_tran_bus/ClarkCountyCTRANBusAgencyTools.java
@@ -5,13 +5,14 @@ import static org.mtransit.commons.StringUtils.EMPTY;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import org.mtransit.commons.CleanUtils;
-import org.mtransit.commons.StringUtils;
+import org.mtransit.parser.ColorUtils;
 import org.mtransit.parser.DefaultAgencyTools;
-import org.mtransit.parser.MTLog;
 import org.mtransit.parser.gtfs.data.GRoute;
 import org.mtransit.parser.gtfs.data.GStop;
 import org.mtransit.parser.mt.data.MAgency;
 
+import java.util.List;
+import java.util.Locale;
 import java.util.regex.Pattern;
 
 // https://www.c-tran.com/about-c-tran/business/c-tran-gtfs-data
@@ -20,6 +21,12 @@ public class ClarkCountyCTRANBusAgencyTools extends DefaultAgencyTools {
 
 	public static void main(@NotNull String[] args) {
 		new ClarkCountyCTRANBusAgencyTools().start(args);
+	}
+
+	@Nullable
+	@Override
+	public List<Locale> getSupportedLanguages() {
+		return LANG_EN;
 	}
 
 	@Override
@@ -38,16 +45,33 @@ public class ClarkCountyCTRANBusAgencyTools extends DefaultAgencyTools {
 		return MAgency.ROUTE_TYPE_BUS;
 	}
 
-	private static final String RSN_VINE = "Vine";
-
-	private static final long VIDE_RID = 50L;
+	@Override
+	public boolean defaultRouteIdEnabled() {
+		return true;
+	}
 
 	@Override
-	public long getRouteId(@NotNull GRoute gRoute) {
-		if (RSN_VINE.equalsIgnoreCase(gRoute.getRouteShortName())) {
-			return VIDE_RID;
+	public boolean useRouteShortNameForRouteId() {
+		return true;
+	}
+
+	@Nullable
+	@Override
+	public Long convertRouteIdFromShortNameNotSupported(@NotNull String routeShortName) {
+		switch (routeShortName) {
+		case "Vine":
+			return 50L;
 		}
-		return Long.parseLong(gRoute.getRouteShortName()); // using route short name as route ID
+		return super.convertRouteIdFromShortNameNotSupported(routeShortName);
+	}
+
+	private static final Pattern STARTS_WITH_0_ = Pattern.compile("(^0+)");
+
+	@NotNull
+	@Override
+	public String cleanRouteShortName(@NotNull String routeShortName) {
+		routeShortName = STARTS_WITH_0_.matcher(routeShortName).replaceAll(EMPTY);
+		return super.cleanRouteShortName(routeShortName);
 	}
 
 	@Override
@@ -69,57 +93,64 @@ public class ClarkCountyCTRANBusAgencyTools extends DefaultAgencyTools {
 	private static final String EXPRESS_ROUTES_COLOR = "ED1A3B"; // RED (from PDF map)
 	private static final String LIMITED_ROUTES_COLOR = "191919"; // "000000" // BLACK (from PDF map)
 
+	@Nullable
+	@Override
+	public String fixColor(@Nullable String color) {
+		if (ColorUtils.BLACK.equalsIgnoreCase(color)) {
+			return null;
+		}
+		return super.fixColor(color);
+	}
+
 	@SuppressWarnings("DuplicateBranchesInSwitch")
 	@Nullable
 	@Override
-	public String getRouteColor(@NotNull GRoute gRoute, @NotNull MAgency agency) {
-		if (StringUtils.isEmpty(gRoute.getRouteColor()) //
-				|| "000000".equalsIgnoreCase(gRoute.getRouteColor())) {
-			if (RSN_VINE.equalsIgnoreCase(gRoute.getRouteShortName())) {
-				return "78BE20";
-			}
-			int rsn = Integer.parseInt(gRoute.getRouteShortName());
-			switch (rsn) {
-			// @formatter:off
-			case 2: return "956338";
-			case 3: return "00A651";
-			case 4: return "B99874";
-			case 6: return "01A553";
-			case 7: return "8DC63F";
-			case 9: return "8781BD";
-			case 19: return "32BCAD";
-			case 25: return "C1B400";
-			case 30: return "BA6831";
-			case 32: return "2E3092";
-			case 37: return "F7941E";
-			case 38: return "B41E8E";
-			case 39: return "EC008E";
-			case 41: return LIMITED_ROUTES_COLOR;
-			case 44: return LIMITED_ROUTES_COLOR;
-			case 47: return LIMITED_ROUTES_COLOR;
-			case 50: return "78BE20"; // VINE
-			case 60: return LIMITED_ROUTES_COLOR;
-			case 65: return LIMITED_ROUTES_COLOR;
-			case 67: return LIMITED_ROUTES_COLOR;
-			case 71: return "FDB913";
-			case 72: return "92278F";
-			case 73: return "BA7031";
-			case 74: return "F573A9";
-			case 78: return "00AEEf";
-			case 80: return "0072BC";
-			case 92: return "BC9632";
-			case 105: return EXPRESS_ROUTES_COLOR;
-			case 134: return EXPRESS_ROUTES_COLOR;
-			case 157: return EXPRESS_ROUTES_COLOR;
-			case 164: return EXPRESS_ROUTES_COLOR;
-			case 177: return EXPRESS_ROUTES_COLOR;
-			case 190: return EXPRESS_ROUTES_COLOR;
-			case 199: return EXPRESS_ROUTES_COLOR;
-			// @formatter:on
-			}
-			throw new MTLog.Fatal("Unexpected route color %s!", gRoute);
+	public String provideMissingRouteColor(@NotNull GRoute gRoute) {
+		switch (getRouteShortName(gRoute)) {
+		// @formatter:off
+		case "2": return "956338";
+		case "3": return "00A651";
+		case "4": return "B99874";
+		case "6": return "01A553";
+		case "7": return "8DC63F";
+		case "9": return "8781BD";
+		case "19": return "32BCAD";
+		case "25": return "C1B400";
+		case "30": return "BA6831";
+		case "32": return "2E3092";
+		case "37": return "F7941E";
+		case "38": return "B41E8E";
+		case "39": return "EC008E";
+		case "41": return LIMITED_ROUTES_COLOR;
+		case "44": return LIMITED_ROUTES_COLOR;
+		case "47": return LIMITED_ROUTES_COLOR;
+		case "Vine": return "78BE20";
+		case "50": return "78BE20"; // VINE
+		case "60": return LIMITED_ROUTES_COLOR;
+		case "65": return LIMITED_ROUTES_COLOR;
+		case "67": return LIMITED_ROUTES_COLOR;
+		case "71": return "FDB913";
+		case "72": return "92278F";
+		case "73": return "BA7031";
+		case "74": return "F573A9";
+		case "78": return "00AEEf";
+		case "80": return "0072BC";
+		case "92": return "BC9632";
+		case "105": return EXPRESS_ROUTES_COLOR;
+		case "134": return EXPRESS_ROUTES_COLOR;
+		case "157": return EXPRESS_ROUTES_COLOR;
+		case "164": return EXPRESS_ROUTES_COLOR;
+		case "177": return EXPRESS_ROUTES_COLOR;
+		case "190": return EXPRESS_ROUTES_COLOR;
+		case "199": return EXPRESS_ROUTES_COLOR;
+		// @formatter:on
 		}
-		return super.getRouteColor(gRoute, agency);
+		return super.provideMissingRouteColor(gRoute);
+	}
+
+	@Override
+	public boolean defaultRouteLongNameEnabled() {
+		return true;
 	}
 
 	@NotNull
